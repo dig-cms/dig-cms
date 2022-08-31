d16e2933bc9e5f62e7e9554652a102fda3c45f2f
Forrest Lyman <forrest@dighub.io>
2022-08-31 06:12:03 -0700

    adds firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCsXhq40XSFcsP9Pmz_LBFW_NT5ydhfVhw",
+    authDomain: "dig-cms-64717.firebaseapp.com",
+    projectId: "dig-cms-64717",
+    storageBucket: "dig-cms-64717.appspot.com",
+    messagingSenderId: "986528470206",
+    appId: "1:986528470206:web:f7c842585e39c94088baef",
+    measurementId: "G-R3W54H68G2"
+  }
 };
 
 /*
